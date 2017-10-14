66e361ca75ffa4ff2d8df03cef5dcaa26c47ee74
Maciej.Trojniarz <maciej.trojniarz@gmail.com>
2017-10-14 21:46:27 +0200

    add fb message handling

---
+++ facebookApi.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+  validation(req, res){
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === "MyRouters") {
+          //Just don't change the bloody password above
+      console.log("Validating webhook");
+      //So the facebook's messenger will be validating this..
+      res.status(200).send(req.query['hub.challenge']);
+      lastResp = 200;
+    } else {
+      console.error("Failed validation. Make sure the validation tokens match.");
+      res.sendStatus(403);
+      lastResp = 403;
+    }
+  },
+
+  callSendAPI(messageData) {
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: 'EAAUynTT85XgBAGWHEGfblN7wn3RwWOv1sAhYtQfMtI022LP3y0osWLrYiCm1FEp1frvcXfPCMKDY1xnIMWoPf8EbETs2DV8EoqgxzQB2ZAhELKUr14JwS3P1YwJhMteGJneGSwwxeu2rHjLNZAZCCZAUq0kAxOgOjNzgWijLaQZDZD' },
+      method: 'POST',
+      json: messageData
+
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+
+        console.log("Successfully sent generic message with id %s to recipient %s",
+          messageId, recipientId);
+      } else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+      }
+    });
+  }
+
+}
--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 var express = require('express');
 var app = express();
 var lastResp = null;
+var facebookApi = require('./facebookApi.js');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/webhook', function(req, res){
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === "MyRouters") {
-        //Just don't change the bloody password above
-    console.log("Validating webhook");
-    //So the facebook's messenger will be validating this..
-    res.status(200).send(req.query['hub.challenge']);
-    lastResp = 200;
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-    lastResp = 403;
-  }
-});
+app.get('/webhook', facebookApi.validation);
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -29,13 +17,69 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/pingme', function(request, response) {
-  response.send("Bite my shiny metal ass..");
-});
-
 app.get('/valid', function(request, response) {
   response.send("Well, last time it:" + lastResp === 200 ? 'Worked' : "Error " + lastResp);
 });
+
+app.post('/webhook', function(request, response) {
+  // response.send('Hi there!!!');
+  facebookApi.callSendAPI("Hi There!!!")
+})
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+
+  if(messageText=='magic'){
+    sendStructuredMessage(senderID);
+  } else {
+    echo(senderID, messageText);
+  }
+
+}
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
