ac3e2c15b8c8b043ef3630a38a74056edeeb37b2
LubieSieUpijacCzskimPiwem <trzcinski.tomasz.1988@gmail.com>
2017-10-14 20:49:23 +0200

    Message - valid - test

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
+var lastResp = null;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/webhook', function(req, res){
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "MyRouters") {
+        //Just don't change the bloody password above
+    console.log("Validating webhook");
+    //So the facebook's messenger will be validating this..
+    res.status(200).send(req.query['hub.challenge']);
+    lastResp = 200;
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+    lastResp = 403;
+  }
+});
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -15,6 +31,10 @@ app.get('/', function(request, response) {
 
 app.get('/pingme', function(request, response) {
   response.send("Bite my shiny metal ass..");
+});
+
+app.get('/valid', function(request, response) {
+  response.send("Well, last time it:" + lastResp == 200 ? 'Worked' : "Error " + lastResp);
 });
 
 app.listen(app.get('port'), function() {
