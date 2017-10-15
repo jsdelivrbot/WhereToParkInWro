acdaffc9dbdd9d37fcc773455bd310c173f1d72e
ttrzcinski <trzcinski.tomasz.1988@gmail.com>
2017-10-15 13:37:23 +0200

    Test of mocked map

---
+++ facebook/theParkListener.js
@@ -0,0 +1,27 @@
+var facebookApi = require('./facebookApi')
+const theParkListener = {
+   
+  run: (message) => {
+    message = 
+    {
+        "recipient": {"id": "132456"},
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": {
+                        "element": {
+                            "title": "Your current location",
+                            "image_url": "https:\/\/maps.googleapis.com\/maps\/api\/staticmap?size=764x400&center=51,17&zoom=25&markers=51,17",
+                            "item_url": "http:\/\/maps.apple.com\/maps?q=51,17&z=16"
+                        }
+                    }
+                }
+            }
+        }
+    },  
+    facebookApi.sendMessage(message.senderId, message.message)}
+}
+
+module.exports = echoServiceListener
